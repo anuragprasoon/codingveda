703acfd693ec7c84155ef74ca4a2a5fbc0e05adc
anuragprasoon <anuragprasoon23@gmail.com>
2023-03-18 00:29:05 +0530

    building courses

--- jdoodleapi.py
+++ jdoodleapi.py
@@ -2,9 +2,9 @@ import pydoodle
 c = pydoodle.Compiler(clientId="d8b8e60770492a2a9324ba34e615b720", clientSecret="98069ff551002310a960986d01c1dec7b2606a4d90bb18444967e456c371f05b")
 str1=r"""
     #include <stdio.h>
-        void main(){
+        int main(){
             int a=110;
-            printf("hello world\n");
+            printf("hello c\n");
             printf("%d",a);
         }"""
 result = c.execute(script=str1, language="c")
